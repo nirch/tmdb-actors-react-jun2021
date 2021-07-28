e6bfe406b110035cf30b37bc269dbbb28cc0c18e
nirch <nirh2@yahoo.com>
2021-07-28 12:17:39 +0300

    SearchBox adding results array and rendering them as li

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-function SearchBox({placeholder, searchText, onSearchChange}) {
+function SearchBox({ placeholder, searchText, onSearchChange, results }) {
     return (
         <div className="c-searchbox">
             <input type="text" className="form-control" placeholder={placeholder}
-                value={searchText} onChange={onSearchChange}/>
+                value={searchText} onChange={onSearchChange} />
+            <ul className="list-group">
+                {results.map((result, index) => 
+                    <li key={index} className="list-group-item list-group-item-action">{result}</li>)}
+            </ul>
         </div>
     );
 }
--- src/pages/ActorsPage/ActorsPage.jsx
+++ src/pages/ActorsPage/ActorsPage.jsx
@@ -5,13 +5,19 @@ import './ActorsPage.css'
 
 function ActorsPage(props) {
     const [serachText, setSearchText] = useState("");
+    const [results, setResults] = useState([
+        "Brad Pitt",
+        "Angelina Jolie",
+        "Marlon Brandom"
+    ]);
 
     return (
         <div className="p-actors">
             <div className="container">
                 <SearchBox placeholder="Search actors..."
                     searchText={serachText}
-                    onSearchChange={e => setSearchText(e.target.value)}/>
+                    onSearchChange={e => setSearchText(e.target.value)}
+                    results={results}/>
                 <ActorCard/>
             </div>
         </div>
