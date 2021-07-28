1a6d79847b03aca0c5c0c9ab80e73dee433907c0
nirch <nirh2@yahoo.com>
2021-07-28 13:35:10 +0300

    adding dummy image to actor

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ActorModel from '../../model/ActorModel';
 
 function ActorCard({ actor }) {
     return (
         <div className="card c-actor" >
-            {/* <img src="..." className="card-img-top" alt="..." /> */}
+            <img src={actor.img} className="card-img-top" alt={actor.name} />
             <div className="card-body">
-                <h5 className="card-title">{ActorModel.name}</h5>
+                <h5 className="card-title">{actor.name}</h5>
                 <p className="card-text">60</p>
                 {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
--- src/model/ActorModel.js
+++ src/model/ActorModel.js
@@ -2,6 +2,7 @@
 export default class ActorModel {
     constructor(name) {
         this.name = name;
+        this.img = "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg";
     }
     
 }
